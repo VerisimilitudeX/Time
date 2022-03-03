3bb7fb1d1a2a01f7fde178c205644b09dfda3198
Verisimilitude <acharyapiyush1@outlook.com>
2022-03-03 12:48:31 -0800

    Update Close Window After 10 Clicks.py
    fixed a bug

--- Close Window After 10 Clicks.py
+++ Close Window After 10 Clicks.py
@@ -7,10 +7,12 @@ frames = 0
 
 while clicks < 10:
     frames += 1
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             clicks = 100
+
         if event.type == pygame.MOUSEBUTTONDOWN:
             clicks += 1
 
-print("It took you " + frames " to click 10 times")
+print("It took you " + str(frames) + " frames to click 10 times")
