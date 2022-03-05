0f2ddb41a8bf99d8cd4c58260aaacb05e90db508
Verisimilitude <acharyapiyush1@outlook.com>
2022-03-04 17:30:05 -0800

    Create Random Colored Circle Art.py

---
+++ Random Colored Circle Art.py
@@ -0,0 +1,44 @@
+# Libraries
+import random
+import pygame
+pygame.init()
+
+# Basic Setup
+window = pygame.display.set_mode([400, 600])
+window.fill((0, 0, 0))
+c = pygame.time.Clock()
+quit = False
+appearance_rate = 2000
+last_circle_appeared = pygame.time.get_ticks()
+
+# Main loop
+while not quit:
+
+    # Event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            quit = True
+
+    # Create new circle if necessary
+    now = pygame.time.get_ticks()
+    if now - last_circle_appeared > appearance_rate:
+
+        # Draw new circle
+        x = random.randint(0, 400)
+        y = random.randint(0, 600)
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        color = (r, g, b)
+        size = random.randint(10, 100)
+        pygame.draw.circle(window, color, (x, y), size)
+
+        # Re-set timing
+        last_circle_appeared += c.get_time()
+        appearance_rate -= 50
+        if appearance_rate < 10:
+            appearance_rate = 10
+
+    # Finish
+    c.tick(30)
+    pygame.display.flip()
